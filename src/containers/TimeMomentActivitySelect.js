7f087ebf73a3e3c991131f4a6c30a437f7af3995
Melinda <melinda.hl.lim@gmail.com>
2020-11-26 14:18:43 -0500

    Tring to add back button - tailwind seems broken

--- src/containers/TimeMomentActivitySelect.js
+++ src/containers/TimeMomentActivitySelect.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
 import ActivitySelection from '../components/ActivitySelection';
 import Button from '../components/Button';
 import Card from '../components/Card';
@@ -36,6 +37,9 @@ function TimeMomentActivitySelect() {
     <div className="min-h-screen flex flex-col items-center bg-orange-100">
 
       <div className="flex flex-col max-w-md w-full flex-grow justify-center px-4 py-8">
+        <div className="fixed top-1 left-1 text-4xl">
+          <IoIosArrowBack />
+        </div>
         <Card>
           <h1 className="text-center text-3xl mx-4">Select an Activity</h1>
           <ActivitySelection
